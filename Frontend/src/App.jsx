16cedea79da8ad88b3ba7385da01668056b16af8
krish mamtora <krishmamtora26@gmail.com>
2025-07-12 11:42:02 +0530

    integrated register userprofile aand send request page along with skill filter in skill section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -34,11 +34,14 @@
 
 // export default App
 
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
+import EditProfilePage from './pages/EditProfilePage';
 import ExplorePage from './pages/ExplorePage';
 import SwapFormPage from './pages/SwapFormPage';
 import RequestsPage from './pages/RequestsPage';
@@ -50,8 +53,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/edit-profile" element={<EditProfilePage />} />
           <Route path="/explore" element={<ExplorePage />} />
           <Route path="/swap-form" element={<SwapFormPage />} />
           <Route path="/requests" element={<RequestsPage />} />
